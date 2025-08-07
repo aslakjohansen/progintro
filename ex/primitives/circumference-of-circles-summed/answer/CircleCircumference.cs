80b0f672f9a8ad401f2c21ad868636a8b529f065
Aslak Johansen <aslakj@gmail.com>
2025-08-07 11:35:22 +0200

    Added an exercise on summed circumferences of circles

---
+++ ex/primitives/circumference-of-circles-summed/answer/CircleCircumference.cs
@@ -0,0 +1,21 @@
+﻿
+const double pi = 3.14;
+double r, c;
+double s = 0.0;
+
+r = 1;
+c = 2.0 * pi * r;
+s += c;
+Console.WriteLine("r=" + r + " -> circumference=" + c);
+
+r = 3;
+c = 2.0 * pi * r;
+s += c;
+Console.WriteLine("r=" + r + " -> circumference=" + c);
+
+r = 5;
+c = 2.0 * pi * r;
+s += c;
+Console.WriteLine("r=" + r + " -> circumference=" + c);
+
+Console.WriteLine("Sum is " + s);
