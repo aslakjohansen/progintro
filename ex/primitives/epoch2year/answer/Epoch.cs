4a8859edad2d28959f0783f909885ae71cba0e67
Aslak Johansen <aslakj@gmail.com>
2025-08-07 19:34:50 +0200

    Added an epoch exercise

---
+++ ex/primitives/epoch2year/answer/Epoch.cs
@@ -0,0 +1,9 @@
+﻿const int secsPerDay = 24 * 60 * 60;
+const int secsPerYear = 365 * secsPerDay;
+
+long epoch = 99023040;
+
+long years = epoch / secsPerYear;
+long days = (epoch - years * secsPerYear) / secsPerDay;
+
+Console.WriteLine(epoch + " seconds -> " + years + " years and " + days + " days");
