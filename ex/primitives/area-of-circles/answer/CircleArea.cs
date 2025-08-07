65b0de2e2bc956f6ed9a2a2524dbc3e7f3099c53
Aslak Johansen <aslakj@gmail.com>
2025-08-07 11:22:46 +0200

    Added an area of circles exercise

---
+++ ex/primitives/area-of-circles/answer/CircleArea.cs
@@ -0,0 +1,12 @@
+﻿
+const double pi = 3.14;
+double r;
+
+r = 1;
+Console.WriteLine("r=" + r + " -> area=" + (pi * r * r));
+
+r = 3;
+Console.WriteLine("r=" + r + " -> area=" + (pi * r * r));
+
+r = 5;
+Console.WriteLine("r=" + r + " -> area=" + (pi * r * r));
