7e3cd784dd5ef23c1a2eca516d8e4651e431b3d7
Aslak Johansen <aslakj@gmail.com>
2025-08-08 18:14:47 +0200

    Added a weekly diff exercise

---
+++ ex/primitives/weekly-temp-diff/answer/DailyDiffs.cs
@@ -0,0 +1,15 @@
+﻿
+double day1 = 21.5;
+double day2 = 23.7;
+double day3 = 19.6;
+double day4 = 22.5;
+double day5 = 25.3;
+double day6 = 21.7;
+double day7 = 18.9;
+
+Console.WriteLine((day2 - day1) + "°C");
+Console.WriteLine((day3 - day2) + "°C");
+Console.WriteLine((day4 - day3) + "°C");
+Console.WriteLine((day5 - day4) + "°C");
+Console.WriteLine((day6 - day5) + "°C");
+Console.WriteLine((day7 - day6) + "°C");
