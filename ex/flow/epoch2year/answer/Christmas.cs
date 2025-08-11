80e068591bf6e47761b082253fe74665d0e15283
Aslak Johansen <aslakj@gmail.com>
2025-08-11 10:44:19 +0200

    Made some readability adjustments

--- ex/flow/epoch2year/answer/Christmas.cs
+++ ex/flow/epoch2year/answer/Christmas.cs
@@ -1,7 +1,6 @@
 const int secsPerDay = 24 * 60 * 60;
 const int secsPerYear = 365 * secsPerDay;
 
-// long epoch = 99023040;
 long epoch = (11 * 30 + 23) * 24 * 60 * 60;
 
 long years = epoch / secsPerYear;
@@ -9,8 +8,6 @@ long days = (epoch - years * secsPerYear) / secsPerDay;
 long month = days / 30;
 long day = days % 30;
 
-// Console.WriteLine("" + (day + 1) + "/" + (month + 1));
-if (month == 11 && day == 23)
-{
-    Console.WriteLine("Det er jul!");
+if (month == 11 && day == 23) {
+  Console.WriteLine("It is Christmas!");
 }
