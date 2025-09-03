394d8e8db2d3a5f1d8511f1664d03dd747306b41
Aslak Johansen <aslakj@gmail.com>
2025-09-03 15:25:49 +0200

    Added an exercise on finding the largest element in an array

---
+++ ex/primdata/largest-in-array/answer/index/Index.cs
@@ -0,0 +1,14 @@
+﻿int[] a = {1, 7, 4, 1, 23, 54, 8, 34, 54, 12, 11, 8, 25};
+
+int max  = a[0];
+int maxi = 0;
+
+for (int i=0 ; i<a.Length ; i++) {
+  int value = a[i];
+  if (value > max) {
+    max  = value;
+    maxi = i;
+  }
+}
+
+Console.WriteLine(maxi);
---
+++ ex/primdata/largest-in-array/answer/indexAll/IndexAll.cs
@@ -0,0 +1,17 @@
+﻿int[] a = {1, 7, 4, 1, 23, 54, 8, 34, 54, 12, 11, 8, 25};
+
+// find max
+int max = a[0];
+foreach (int value in a) {
+  if (value > max) {
+    max = value;
+  }
+}
+
+// locate occurrences and output them
+for (int i=0; i<a.Length ; i++) {
+  int value = a[i];
+  if (value==max) {
+    Console.WriteLine(i);
+  }
+}
