d39a76806c6db97928f28d7d9b8879234088c66d
Aslak Johansen <aslakj@gmail.com>
2025-09-04 09:42:27 +0200

    Added a multiplication table exercise

---
+++ ex/primdata/mult-table/answer/MultiplicationTable.cs
@@ -0,0 +1,16 @@
+﻿const int multiplier = 3;
+const int count = 10;
+
+// allocate memory
+int[] data = new int[count];
+
+// populate array
+for (int i=0 ; i<data.Length ; i++) {
+  data[i] = i*multiplier;
+}
+
+// perform test
+int test1 = 0;
+int test2 = count - 1;
+Console.WriteLine(test1+" * "+multiplier+" = "+data[test1]);
+Console.WriteLine(test2+" * "+multiplier+" = "+data[test2]);
