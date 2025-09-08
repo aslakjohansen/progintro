85fa2582a94384eb4bfcaaf8066b4542a4956e60
Aslak Johansen <aslakj@gmail.com>
2025-09-08 10:57:08 +0200

    Added an array reference exercise

---
+++ ex/primdata/leap-year-pointer/answer/Calendar.cs
@@ -0,0 +1,13 @@
+﻿int[] monthsNormal = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int[] monthsLeap   = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+for (int year=2000 ; year<=2020 ; year++) {
+  bool leap = (year%4==0 && year%100!=0) || (year%400==0);
+  int[] month = leap ? monthsLeap : monthsNormal;
+  
+  Console.Write(year + ": [ ");
+  foreach (int length in month) {
+    Console.Write(length + " ");
+  }
+  Console.WriteLine("]");
+}
