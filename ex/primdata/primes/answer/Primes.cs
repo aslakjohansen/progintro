82f7580c149b881448999290cdaf6d58096e03d6
Aslak Johansen <aslakj@gmail.com>
2025-09-08 10:30:32 +0200

    Fixed reference

---
+++ ex/primdata/primes/answer/Primes.cs
@@ -0,0 +1,25 @@
+﻿int last = 1000000;
+bool[] sieve = new bool[last];
+
+// initialize sieve
+for (int i=1 ; i<sieve.Length ; i++)
+  sieve[i] = true;
+
+// fill out sieve
+for (int i=2 ; i<Math.Sqrt(sieve.Length) ; i++) {
+  if (sieve[i]) {
+    for (int j=i*2 ; j<last ; j+=i) {
+      sieve[j] = false;
+    }
+  }
+}
+
+// find and print out largest prime
+int max = -1;
+for (int i=last-1 ; i>0 ; i--) {
+  if (sieve[i]) {
+    max = i;
+    break;
+  }
+}
+Console.WriteLine("Highest prime is "+max);
