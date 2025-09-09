5884d5bfc900cfe9567c7c58814666d5fcfb61e8
Aslak Johansen <aslakj@gmail.com>
2025-09-09 15:14:38 +0200

    Added an input parsing exception handling exercise

---
+++ ex/exceptions/accounts/answer/Accounts.cs
@@ -0,0 +1,30 @@
+﻿int[] accounts = {903, 716, 67};
+
+int GetAccountNumber ()
+{
+  Console.WriteLine("Enter an account number: ");
+  while (true) {
+    try {
+      return Convert.ToInt32(Console.ReadLine());
+    } catch (FormatException) {
+      Console.WriteLine("I'm afraid that I don't understand. Please try again: ");
+    }
+  }
+  
+}
+
+void PrintAccountState (int accountId)
+{
+  Console.WriteLine("Account " + accountId + " contains " + accounts[accountId]);
+}
+
+
+while (true)
+{
+  int accountId = GetAccountNumber();
+  try {
+    PrintAccountState(accountId);
+  } catch (IndexOutOfRangeException) {
+    Console.WriteLine("Unknown account: " + accountId);
+  }
+}
---
+++ ex/exceptions/accounts/question/Accounts.cs
@@ -0,0 +1,17 @@
+﻿int[] accounts = {903, 716, 67};
+
+int GetAccountNumber ()
+{
+  Console.WriteLine("Enter an account number: ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintAccountState (int accountId)
+{
+  Console.WriteLine("Account " + accountId + " contains " + accounts[accountId]);
+}
+
+while (true) {
+  int accountId = GetAccountNumber();
+  PrintAccountState(accountId);
+}
