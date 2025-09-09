852c0451a300e9b5018a64e11a712c4c75cc4f74
Aslak Johansen <aslakj@gmail.com>
2025-09-09 11:29:55 +0200

    Added a simple exercise in creating and add function

---
+++ ex/functions/sum/answer/Adder.cs
@@ -0,0 +1,10 @@
+﻿int Add (int a, int b) {
+  return a + b;
+}
+
+for (int a=0 ; a<=10 ; a++) {
+  for (int b=0 ; b<=10 ; b++) {
+    Console.Write("{0,3}", Add(a, b));
+  }
+  Console.WriteLine();
+}
