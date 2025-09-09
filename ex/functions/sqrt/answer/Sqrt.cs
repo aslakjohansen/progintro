31d3312fcb03864e8d4e46d056b46ae98781e8f2
Aslak Johansen <aslakj@gmail.com>
2025-09-09 13:12:45 +0200

    Added a square root exercise

---
+++ ex/functions/sqrt/answer/Sqrt.cs
@@ -0,0 +1,19 @@
+﻿double square_root(double x) {
+  double candidate = 0.0;
+  
+  for (double pointer = 1000000000.0 ; pointer > 0.000000001 ; pointer /= 10) {
+    candidate += 10 * pointer;
+    for (int i = 0 ; i < 10 ; i++) {
+      candidate -= pointer;
+      if (candidate * candidate < x) {
+        break;
+      }
+    }
+  }
+  
+  return candidate;
+}
+
+for (double x = 1; x <= 10; x++) {
+  Console.WriteLine("sqrt(" + x + ")=" + square_root(x));
+}
