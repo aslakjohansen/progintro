2cfb10c947b1f908c8abe0fe0bcd41e58ee2eb03
Aslak Johansen <aslakj@gmail.com>
2025-09-09 13:46:19 +0200

    Added progression of the properties of circles exercise

---
+++ ex/functions/properties-of-circles/answer/Circles.cs
@@ -0,0 +1,13 @@
+﻿const double pi = 3.14;
+
+double area (double r) {
+  return pi * r * r;
+}
+
+double circumference (double r) {
+  return 2 * pi * r;
+}
+
+for (double r = 1 ; r <= 5 ; r += 2) {
+  Console.WriteLine("r="+r+" => area="+area(r)+" circ="+circumference(r));
+}
