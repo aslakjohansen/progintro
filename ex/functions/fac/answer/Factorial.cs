4c45e98f06b69c5897166257808f4f33597106cc
Aslak Johansen <aslakj@gmail.com>
2025-09-09 13:32:34 +0200

    Added an exercise in producing a faculty function using recursion

---
+++ ex/functions/fac/answer/Factorial.cs
@@ -0,0 +1,15 @@
+﻿int fac (int i) {
+  if (i < 0) {
+    Console.WriteLine("That was not very nice of you!");
+  }
+  
+  if (i > 0) {
+    return i * Fac(i - 1);
+  } else {
+    return 1;
+  }
+}
+
+for (int i = 1 ; i < 10 ; i++) {
+  Console.WriteLine(" fac(" + i + ") = " + fac(i));
+}
