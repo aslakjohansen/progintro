1549d38d15e0190e6fe0b191e9bcc5ba85659d50
Aslak Johansen <aslakj@gmail.com>
2025-09-09 15:44:03 +0200

    Added an average grade exercise

---
+++ ex/exceptions/avg-grade/answer/AvgGradeException.cs
@@ -0,0 +1,24 @@
+﻿int[] grades = {4, 7, 02, 00, 10, 4, 12};
+
+int GetGrade (int courseId)
+{
+  int grade = grades[courseId];
+  if (grade >= 02) {
+    return grade;
+  } else {
+    throw new Exception(); // this is an exceptional grade!
+  }
+}
+
+int count = 0;
+int sum   = 0;
+
+for (int courseId=0 ; courseId<grades.Length ; courseId++) {
+  try {
+    sum += GetGrade(courseId);
+    count++;
+  } catch (Exception) {
+  }
+}
+
+Console.WriteLine("Average grade is " + ((float)sum / count));
