101608575f993cc963a49832c1d890844744f6e8
Aslak Johansen <aslakj@gmail.com>
2025-10-15 12:04:28 +0200

    Added an command line argument exercise

---
+++ ex/io/cla/answer/CLA.cs
@@ -0,0 +1,20 @@
+﻿public class CLA
+{
+  public static void Main (string[] args) {
+    // guard: command line arguments
+    if (args.Length != 2) {
+      Console.WriteLine("Syntax: dotnet run FIRST_INTEGER SECOND_INTEGER");
+      Console.WriteLine("        dotnet run 42 56");
+      return;
+    }
+    
+    try {
+      int a = int.Parse(args[0]);
+      int b = int.Parse(args[1]);
+      
+      Console.WriteLine(a + b);
+    } catch (FormatException e) {
+      Console.WriteLine("Eww, that's nasty!\n" + e.Message);
+    }
+  }
+}
