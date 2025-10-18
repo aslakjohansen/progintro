edacef5f84b7b9b3803422f647b6ee1c745987d9
Aslak Johansen <aslakj@gmail.com>
2025-10-18 11:16:56 +0200

    Added exercise on writing to a file

---
+++ ex/io/fileoutput/answer/Months.cs
@@ -0,0 +1,36 @@
+﻿public class Months
+{
+  static string[] months = {
+    "Januar",
+    "Februar",
+    "Marts",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "December",
+  };
+  
+  static bool Write (string filename) {
+    try {
+      using (StreamWriter writer = new StreamWriter(filename)) {
+        for (int i=0 ; i<months.Length ; i++) {
+          writer.WriteLine((i + 1) + ":" + months[i]);
+        }
+      }
+      
+      return true;
+    } catch (IOException) {
+      return false;
+    }
+  }
+  
+  public static void Main (string[] args) {
+    bool success = Write("output.txt");
+    Console.WriteLine("Operation was" + (success ? " " : " not ") + "successful");
+  }
+}
