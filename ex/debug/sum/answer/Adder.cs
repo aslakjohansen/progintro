4518f35756c22ad915c0575457b091d3c2c091db
Aslak Johansen <aslakj@gmail.com>
2025-10-25 13:15:21 +0200

    Added a sum exercise

---
+++ ex/debug/sum/answer/Adder.cs
@@ -0,0 +1,18 @@
+public class Adder
+{
+  public static int Sum (int[] numbers) {
+    int result = 0;
+    for (int i=0 ; i<numbers.Length ; i++) {
+      result += numbers[i];
+    }
+    return result;
+  }
+  
+  public static void Main (String[] args) {
+    int[] testcase1 = {1, 3, 5, 2, 3, 7};
+    int[] testcase2 = {5, 6, 2, 3};
+    
+    Console.WriteLine(Sum(testcase1));
+    Console.WriteLine(Sum(testcase2));
+  }
+}
