c0398faa3b0592d2a2cf586bb9cde01fd8d3e353
Aslak Johansen <aslakj@gmail.com>
2026-02-25 22:17:53 +0100

    Cleanup

--- src/csharp/linesegment/LineSegment.cs
+++ src/csharp/linesegment/LineSegment.cs
@@ -1,11 +1,8 @@
 class LineSegment<T> where T :Point<T> {
   private T endpointA;
   private T endpointB;
-  // private Point<T> endpointA;
-  // private Point<T> endpointB;
   
-  // public LineSegment (Point<T> endpointA, Point<T> endpointB) {
-    public LineSegment (T endpointA, T endpointB) {
+  public LineSegment (T endpointA, T endpointB) {
     this.endpointA = endpointA;
     this.endpointB = endpointB;
   }
--- src/csharp/linesegment/Point.cs
+++ src/csharp/linesegment/Point.cs
@@ -1,4 +1,3 @@
 interface Point<T> {
-  // double DistanceTo (Point<T> other);
   double DistanceTo (T other);
 }
