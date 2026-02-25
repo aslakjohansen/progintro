77d3a734b4252b81d959c2d9c0a983fcb25a5ac0
Aslak Johansen <aslakj@gmail.com>
2026-02-25 22:23:33 +0100

    Added a 3D point and converted main program into something that works on the line segments

--- src/csharp/linesegment/Program.cs
+++ src/csharp/linesegment/Program.cs
@@ -1,4 +1,9 @@
-﻿Point2D p0 = new Point2D(0, 0);
-Point2D p1 = new Point2D(1, 1);
-LineSegment<Point2D> l = new LineSegment<Point2D>(p0, p1);
-Console.WriteLine(p0.DistanceTo(p1));
+﻿Point2D p20 = new Point2D(0, 0);
+Point2D p21 = new Point2D(1, 1);
+LineSegment<Point2D> l2 = new LineSegment<Point2D>(p20, p21);
+Console.WriteLine(l2.GetLength());
+
+Point3D p30 = new Point3D(0, 0, 0);
+Point3D p31 = new Point3D(1, 1, 1);
+LineSegment<Point3D> l3 = new LineSegment<Point3D>(p30, p31);
+Console.WriteLine(l3.GetLength());
