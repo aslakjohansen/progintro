2916d3e2a57f2cf7a3dc8e11c8ed05d37a8bcab3
Aslak Johansen <aslakj@gmail.com>
2026-03-27 22:45:33 +0100

    First steps in the direction of a script to process code snippets
    Current features:
    - locate latex file inclusions

---
+++ bin/process-code.exs
@@ -0,0 +1,68 @@
+#!/usr/bin/env elixir
+
+filename = "../doc/document.tex"
+
+defmodule ScanEntry do
+  defstruct re: nil, handler: nil, context: nil
+end
+
+defmodule Scanner do
+  @spec match(list(ScanEntry), list(ScanEntry), binary(), list(any())) :: list(any())
+  defp match(_rest, _all, "", output), do: output
+
+  defp match([], all, input, output) do
+    match(all, all, String.slice(input, 1..-1//1), output)
+  end
+
+  defp match([first | rest], all, input, output) do
+    %{re: re, handler: handler, context: context} = first
+
+    r = Regex.run(re, input)
+
+    if r == nil do
+      match(rest, all, input, output)
+    else
+      l = r |> Enum.at(0) |> String.length()
+      result = handler.(context, r)
+      output = [result | output]
+      input = String.slice(input, l..-1//1)
+      match(all, all, input, output)
+    end
+  end
+
+  def process(machine, input) do
+    match(machine, machine, input, [])
+    |> Enum.reverse()
+  end
+end
+
+defmodule Script do
+  defp parse(contents) do
+    machine = [
+      %ScanEntry{
+        re: ~r/^\\input{([^}#]+)}/,
+        handler: fn _context, [_ | [filename | _]] -> filename end
+      }
+    ]
+
+    Scanner.process(machine, contents)
+  end
+
+  defp process(filename) do
+    IO.puts("Processing '#{filename}'")
+
+    filename
+    |> File.read!()
+    |> parse()
+  end
+
+  def run(filename) do
+    jobs =
+      filename
+      |> process()
+
+    IO.inspect(jobs)
+  end
+end
+
+Script.run(filename)
