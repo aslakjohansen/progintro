a4d8908d5eb1d2df9ebc7c61c2ef49481dd314d5
Aslak Johansen <aslakj@gmail.com>
2026-05-28 13:22:15 +0200

    Updated example to use hidden definition of the incr function

--- src/elixir/functions/map/map.exs
+++ src/elixir/functions/map/map.exs
@@ -1,5 +1,7 @@
 #!/usr/bin/env elixir
 
+incr = fn value -> value + 1 end
+
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-|> Enum.map(fn value -> value + 1 end)
+|> Enum.map(incr)
 |> IO.inspect(charlists: :as_lists)
