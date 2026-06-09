ea892158c96b168bd7256c0e5f8f48f50d9370d5
Aslak Johansen <aslakj@gmail.com>
2026-06-09 12:57:21 +0200

    Added a bit more material to the months example

--- src/csharp/primdata/months/Program.cs
+++ src/csharp/primdata/months/Program.cs
@@ -1,5 +1,11 @@
 int[] months = new int[] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+for (int i=0 ; i<months.Length ; i++) {
+  Console.WriteLine(i+": "+months[i]);
+}
+
+Console.WriteLine("");
+
 months[1] = 42;
 months[3] = months[1];
 
