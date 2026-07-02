5cc6384146f244fb6b29675029cf2e8f4eb3b590
Aslak Johansen <aslakj@gmail.com>
2026-07-02 08:47:01 +0200

    Added program illustrating if chaining

---
+++ src/csharp/flow_branch/chain/Program.cs
@@ -0,0 +1,13 @@
+char direction = 'N';
+
+if (direction=='N') {
+  Console.WriteLine("Going north ...");
+} else if (direction=='S') {
+  Console.WriteLine("Going south ...");
+} else if (direction=='E') {
+  Console.WriteLine("Going east ...");
+} else if (direction=='W') {
+  Console.WriteLine("Going west ...");
+} else {
+  Console.WriteLine("I don't understand ?!?");
+}
