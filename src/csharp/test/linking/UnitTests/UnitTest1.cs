eaa8b1a8c00e36911506eb606f2491f154db8fdb
Aslak Johansen <aslakj@gmail.com>
2026-09-21 12:36:37 +0200

    Included test code and output

--- src/csharp/test/linking/UnitTests/UnitTest1.cs
+++ src/csharp/test/linking/UnitTests/UnitTest1.cs
@@ -2,14 +2,12 @@ namespace UnitTests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+  [SetUp]
+  public void Setup () {
+  }
+  
+  [Test]
+  public void Test1 () {
+    Assert.That(Util.Add(1,2), Is.EqualTo(4));
+  }
 }
