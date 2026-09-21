76da40126a4fd5d97a230e59e28ceb5a035e8d39
Aslak Johansen <aslakj@gmail.com>
2026-09-21 16:55:23 +0200

    A bit of restructuring and fillout

--- src/csharp/test/linking/UnitTests/UtilTest.cs
+++ src/csharp/test/linking/UnitTests/UtilTest.cs
@@ -1,11 +1,7 @@
 namespace UnitTests;
 
-public class Tests
+public class UtilTest
 {
-  [SetUp]
-  public void Setup () {
-  }
-  
   [Test]
   public void Test1 () {
     Assert.That(Util.Add(1,2), Is.EqualTo(4));
